c36fc16edb9acf63958ab03cdfcb31d70715c7f8
Burke Libbey <burke@libbey.me>
2012-10-29 18:29:22 -0500

    Refactored scheduler to make it a bit more testable.

--- lib/multi_db/scheduler.rb
+++ lib/multi_db/scheduler.rb
@@ -1,38 +1,42 @@
+require 'tlattr_accessors'
+require 'active_support/core_ext/module/delegation'
+
 module MultiDb
   class Scheduler
-    class NoMoreItems < Exception; end
+    NoMoreItems = Class.new(Exception)
     extend ThreadLocalAccessors
 
     attr :items
-    delegate :[], :[]=, :to => :items
+    delegate :[], :[]=, to: :items
     tlattr_accessor :current_index, true
-    
-    def initialize(items, blacklist_timeout = 30.seconds)
+
+    def initialize(items, blacklist_timeout = 30)
       @n = items.length
       @items     = items
       @blacklist = Array.new(@n, Time.at(0))
       @blacklist_timeout = blacklist_timeout
       self.current_index = rand(@n)
     end
-    
+
     def blacklist!(item)
       @blacklist[@items.index(item)] = Time.now
     end
-    
+
     def current
       @items[current_index]
     end
-    
+
     def next
       previous = current_index
-      until(@blacklist[next_index!] < Time.now - @blacklist_timeout) do
+      threshold = Time.now - @blacklist_timeout
+      until(@blacklist[next_index!] < threshold) do
         raise NoMoreItems, 'All items are blacklisted' if current_index == previous
       end
       current
     end
-    
+
     protected
-    
+
     def next_index!
       self.current_index = (current_index + 1) % @n
     end
