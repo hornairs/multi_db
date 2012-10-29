90e642ede77a0151abf557e92040c6389561fa6a
Burke Libbey <burke@libbey.me>
2012-10-29 18:32:59 -0500

    Added LagMonitor specs

---
+++ spec/lag_monitor_spec.rb
@@ -0,0 +1,44 @@
+require './lib/multi_db/lag_monitor'
+
+describe MultiDb::LagMonitor do
+
+  describe "sticky_master_duration" do
+
+    it "returns 1 second even when there is no replica lag" do
+      subject.stub(slave_lag: 0)
+      subject.sticky_master_duration(anything).should == 1
+    end
+
+    it "pads a bit" do
+      subject.stub(slave_lag: 1)
+      subject.sticky_master_duration(anything).should == 2
+
+      subject.stub(slave_lag: 2)
+      subject.sticky_master_duration(anything).should == 3
+
+      subject.stub(slave_lag: 3)
+      subject.sticky_master_duration(anything).should == 5
+    end
+
+  end
+
+  describe "replication_lag_too_high?" do
+
+    it "is false it the lag is zero" do
+      subject.stub(slave_lag: 0)
+      subject.replication_lag_too_high?(anything).should be_false
+    end
+
+    it "is false it the lag is reasonable" do
+      subject.stub(slave_lag: 10)
+      subject.replication_lag_too_high?(anything).should be_false
+    end
+
+    it "is true it the lag is too high" do
+      subject.stub(slave_lag: 11)
+      subject.replication_lag_too_high?(anything).should be_true
+    end
+
+  end
+
+end
